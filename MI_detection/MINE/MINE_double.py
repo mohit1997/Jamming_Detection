d5f79991acf68e35d6fcefabef7557900e1cff88
mohit1997 <goyal.mohit999@gmail.com>
2018-12-28 00:58:43 +0530

    MINE Decoder implemented and results updated

--- MI_detection/MINE/MINE_double.py
+++ MI_detection/MINE/MINE_double.py
@@ -5,11 +5,6 @@ import matplotlib.pyplot as plt
 from sklearn.feature_selection import mutual_info_regression
 from sklearn.feature_selection import mutual_info_classif
 from utils import *
-import os.path
-import pandas as pd
-import csv
-import argparse
-
 
 n_epochs = 1000
 
@@ -19,18 +14,7 @@ def lr(epoch):
     if epoch <500:
         return 1e-3
     else:
-        return 1e-4
-
-def get_threshold(l1, l2):
-    m1 = np.mean(l1)
-    v1 = np.std(l1)
-
-    m2 = np.mean(l2)
-    v2 = np.std(l2)
-
-    thresh = (v2*m1 + v1*m2)/(v1+v2)
-
-    return thresh
+        return 5e-4
 
 def gaussian_noise_layer(input_layer, std, is_train):
     noise = tf.random_normal(shape=tf.shape(input_layer), mean=0.0, stddev=std, dtype=tf.float32)
@@ -65,25 +49,24 @@ def MINE(x_in, y_in, is_train, H, lr):
 
     return neg_loss, opt
 
-def main(args):
+def main():
     ### Generate Data
-    prob = args.prob
-    snr = args.snr
-    window = args.window
-    test_points = 10000
-    X, Y1, Y2 = gen_data(p=prob, SNR=snr, N=100000, A=1)
+    X, Y1, Y2 = gen_data(p=0.5, SNR=0.0, N=100000, A=1)
 
     ### Get MI
     mi_numerical = mutual_info_regression(Y1.reshape(-1, 1), Y2.ravel())[0]
     print(mi_numerical, " during attack")
     mia = mi_numerical
 
-    X, Y1, Y2 = gen_data(p=prob, SNR=snr, N=100000, A=0)
+    X, Y1, Y2 = gen_data(p=0.5, SNR=0.0, N=100000, A=0)
 
     ### Get MI
     mi_numerical = mutual_info_regression(Y1.reshape(-1, 1), Y2.ravel())[0]
     print(mi_numerical, " during no attack")
     mina = mi_numerical
+
+    Y1 = Y1.reshape(-1, 1)
+    Y2 = Y2.reshape(-1, 1)
 
     # prepare the placeholders for inputs
     x_in = tf.placeholder(tf.float32, [None, 1], name='x_in')
@@ -106,38 +89,22 @@ def main(args):
         # generate the data
         # x_sample=gen_x()
         # y_sample=gen_y(x_sample)
-        _, Y1A, Y2A = gen_data(p=prob, SNR=snr, N=window, A=1)
-        Y1A = Y1A.reshape(-1, 1)
-        Y2A = Y2A.reshape(-1, 1)
-        _, Y1NA, Y2NA = gen_data(p=prob, SNR=snr, N=window, A=0)
-        Y1NA = Y1NA.reshape(-1, 1)
-        Y2NA = Y2NA.reshape(-1, 1)
-
-        Y1 = np.concatenate([Y1A, Y1NA], axis=0)
-        Y2 = np.concatenate([Y2A, Y2NA], axis=0)
-        # perform the training step
-        feed_dict = {x_in:Y1, y_in:Y2, is_train: True, learning_rate: lr(epoch)}
-        _, neg_l = sess.run([opt, neg_loss], feed_dict=feed_dict)
-        
-        # save the loss
-        _, Y1, Y2 = gen_data(p=prob, SNR=snr, N=window, A=1)
+        _, Y1, Y2 = gen_data(p=0.5, SNR=0.0, N=100, A=1)
         Y1 = Y1.reshape(-1, 1)
         Y2 = Y2.reshape(-1, 1)
-        
         # perform the training step
-        feed_dict = {x_in:Y1, y_in:Y2, is_train: False, learning_rate: lr(epoch)}
+        feed_dict = {x_in:Y1, y_in:Y2, is_train: True, learning_rate: lr(epoch)}
         neg_l, = sess.run([neg_loss], feed_dict=feed_dict)
         
         # save the loss
         MIAs.append(-neg_l)
 
-        _, Y1, Y2 = gen_data(p=prob, SNR=snr, N=window, A=0)
+        _, Y1, Y2 = gen_data(p=0.5, SNR=0.0, N=100, A=0)
         Y1 = Y1.reshape(-1, 1)
         Y2 = Y2.reshape(-1, 1)
-        
         # perform the training step
-        feed_dict = {x_in:Y1, y_in:Y2, is_train: False, learning_rate: lr(epoch)}
-        neg_l, = sess.run([neg_loss], feed_dict=feed_dict)
+        feed_dict = {x_in:Y1, y_in:Y2, is_train: True, learning_rate: lr(epoch)}
+        _, neg_l = sess.run([opt, neg_loss], feed_dict=feed_dict)
         
         # save the loss
         MINAs.append(-neg_l)
@@ -145,73 +112,17 @@ def main(args):
     feed_dict = {x_in:Y1, y_in:Y2, is_train: False}
     neg_l, = sess.run([neg_loss], feed_dict=feed_dict)
 
-    A_list = MIAs[-400:]
-    NA_list = MINAs[-400:]
-
-    thresh = get_threshold(A_list, NA_list)
-
     print(-neg_l)
         
     fig, ax = plt.subplots()
     ax.plot(range(len(MIAs)), MIAs, label='MINE estimate - Attack')
     ax.plot(range(len(MINAs)), MINAs, label='MINE estimate - NoAttack')
     ax.plot([0, len(MIAs)], [mia,mia], label='True Mutual Information - Attack')
-    ax.plot([0, len(MIAs)], [thresh, thresh], label='Threshold for classification')
     ax.plot([0, len(MINAs)], [mina,mina], label='True Mutual Information - NoAttack')
     ax.set_xlabel('training steps')
     ax.legend(loc='best')
-    title = "SNR=" + str(snr) + "window=" + str(window) + "prob=" + str(prob)
-    ax.set_title(title)
-    path = "figs/MINE_shuffled" + title + ".png"
-    fig.savefig(path)
-    # fig.show()
-
-    misdetections = 0
-    false_alrams = 0
-
-    for i in range(test_points):
-        _, valY1, valY2 = gen_data(p=prob, SNR=snr, N=window, A=0)
-        feed_dict = {x_in:valY1, y_in:valY2, is_train: False}
-        neg_l, = sess.run([neg_loss], feed_dict=feed_dict)
-        MI = -neg_l
-        if MI < thresh:
-            misdetections += 1
-
-        _, valY1, valY2 = gen_data(p=prob, SNR=snr, N=window, A=1)
-        feed_dict = {x_in:valY1, y_in:valY2, is_train: False}
-        neg_l, = sess.run([neg_loss], feed_dict=feed_dict)
-        MI = -neg_l
-        if MI > thresh:
-            false_alrams += 1
-
-    MR = misdetections*1.0 / test_points
-    FAR = false_alrams*1.0 / test_points
-
-    output = "SNR=" + str(snr) + " window=" + str(window) + " prob=" + str(prob) + " MR=" + str(MR) + " FAR" + str(FAR)
-
-    print(output)
-    res = [snr, window, prob, MR, FAR]
-
-     
-    with open('results.csv', 'a') as myFile:
-        writer = csv.writer(myFile)
-        writer.writerow(res)
-
-    sess.close()
-
+    fig.savefig('MINE_both.png')
+    fig.show()
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-snr', action='store', type=int, default=0,
-                        dest='snr',
-                        help='choose sequence file')
-    parser.add_argument('-p', action='store', type=int, default=0.5,
-                        dest='prob',
-                        help='choose input probability')
-    parser.add_argument('-w', action='store', type=int, default=50,
-                        dest='window',
-                        help='window size')
-    arguments = parser.parse_args()
-    print(arguments)
-    main(arguments)
-
+    main()
